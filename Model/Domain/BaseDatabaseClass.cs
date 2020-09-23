852576cb86233e21c4660ca4bf7bea573aefbcf2
Raf Ceuls <rceuls@gmail.com>
2020-09-23 17:16:13 +0200

    fixed the comments

--- Model/Domain/BaseDatabaseClass.cs
+++ Model/Domain/BaseDatabaseClass.cs
@@ -2,7 +2,6 @@ namespace BasicRestAPI.Models.Domain
 {
     public abstract class BaseDatabaseClass
     {
-        // contains the primary key of the database
         public int Id { get; set; }
     }
 }
