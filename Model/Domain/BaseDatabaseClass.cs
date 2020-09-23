66bf26dc257ae296294b448449d6d1814abff044
Raf Ceuls <rceuls@gmail.com>
2020-09-23 17:14:12 +0200

    Comment added to baseclass

--- Model/Domain/BaseDatabaseClass.cs
+++ Model/Domain/BaseDatabaseClass.cs
@@ -2,6 +2,7 @@ namespace BasicRestAPI.Models.Domain
 {
     public abstract class BaseDatabaseClass
     {
+        // contains the primary key of the database
         public int Id { get; set; }
     }
 }
