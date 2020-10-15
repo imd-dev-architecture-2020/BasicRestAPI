8d14170175c08c58958208c354db320f64342a35
Raf Ceuls <rceuls@gmail.com>
2020-10-15 09:08:11 +0200

    added more documentation via swagger

--- BasicRestAPI.Core/Model/Web/CarWebOutput.cs
+++ BasicRestAPI.Core/Model/Web/CarWebOutput.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using BasicRestAPI.Model.Domain;
 
 namespace BasicRestAPI.Model.Web
@@ -11,7 +12,12 @@ namespace BasicRestAPI.Model.Web
             Brand = brand;
         }
 
+        /// <summary>
+        /// This is an identifier that is guaranteed to be unique within the context of a single instance of our API,
+        /// </summary>
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public Brand Brand { get; set; }
     }
